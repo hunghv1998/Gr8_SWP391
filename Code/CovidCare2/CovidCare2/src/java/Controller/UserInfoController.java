bae1ff1786789b8bcdc4632c0f7fceca804280ae
chinhldc <chinhldse04423@fpt.edu.vn>
2022-02-24 01:13:13 +0700

    [chinhld] update profile

--- Code/CovidCare2/CovidCare2/src/java/Controller/UserInfoController.java
+++ Code/CovidCare2/CovidCare2/src/java/Controller/UserInfoController.java
@@ -20,7 +20,11 @@ import java.io.OutputStream;
 import java.sql.Date;
 import java.sql.Timestamp;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
+
+import utils.ValidatingInput;
+import utils.UploadFile;
 
 /**
  *
@@ -33,31 +37,43 @@ import javax.servlet.http.Part;
 )
 
 public class UserInfoController extends HttpServlet {
+
     private static final long SerialVersionUID = 1L;
-    private static final String  UPLOAD_DIR = "images/uploads";
+    private static final String UPLOAD_DIR = "images/uploads";
+
+    private final UserInfoDAO uiD = new UserInfoDAO();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Get session
+        HttpSession session = request.getSession();
+        
+        // Set UserInfo title
         if (request.getParameter("update") != null) {
             request.setAttribute("title", "Cập nhật thông tin cá nhân");
         } else {
             request.setAttribute("title", "Thông tin các nhân");
         }
-        
+        // Authentication
         Account account = (Account) request.getSession().getAttribute("account");
 
         if (account == null) {
             response.sendRedirect("Login");
         } else {
-            UserInfo user = new UserInfo();
-            UserInfoDAO uiD = new UserInfoDAO();
+            UserInfo userinfo = new UserInfo(null, false, "","", "", "", false);
             if (!uiD.checkFirstLogin(account.getUserName())) {
                 request.setAttribute("update", "update");
             } else {
-                user = uiD.getUserInfo(account.getUserName());
+                userinfo = uiD.getUserInfo(account.getUserName());
             }
-            request.getSession().setAttribute("userinfo", user);
+            session.setAttribute("name", userinfo.getName());
+            session.setAttribute("gender", userinfo.isSex());
+            session.setAttribute("bod", userinfo.getBday());
+            session.setAttribute("email", userinfo.getEmail());
+            session.setAttribute("image", userinfo.getImage());
+            session.setAttribute("address", userinfo.getAddress());
+            request.getSession().setAttribute("userinfo", userinfo);
             request.getRequestDispatcher("/view/profile.jsp").forward(request, response);
         }
     }
@@ -66,34 +82,75 @@ public class UserInfoController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         if (request.getParameter("update") != null) {
+            //Get UserInfo from session
+            UserInfo userinfo = (UserInfo) request.getSession().getAttribute("userinfo");
+            if (userinfo == null) {
+                userinfo = new UserInfo();
+            }
+
             //get info from jsp
             String name = request.getParameter("name");
             String email = request.getParameter("email");
-//            String image = request.getParameter("image");
-            String image = uploadFile(request, "profilepic");
-            
+            String phone = request.getParameter("phone");
             String address = request.getParameter("address");
+            Date bod = Date.valueOf(request.getParameter("bod"));
+            //Image processing
+            Part filePart = request.getPart("profilepic");
+
+//            String image = request.getParameter("image");
+            String image = "";
+
             System.out.println(request.getParameter("bod"));
-            Date bday = Date.valueOf(request.getParameter("bod"));
-            boolean sex = true;
+
+            boolean gender = true;
             if (request.getParameter("gender").equals('1')) {
-                sex = false;
+                gender = false;
             }
 
             // Validating input
-            // Update input
-            UserInfo userinfo = new UserInfo(bday, sex, email, image, address, name, true);
-            Account account = (Account) request.getSession().getAttribute("account");
-            UserInfoDAO uiD = new UserInfoDAO();
-            int status = uiD.UpdateUserInfo(userinfo, account.getUserName());
-            if (status > 0) {
-                request.getSession().setAttribute("userinfo", userinfo);
-                response.sendRedirect("UserInfo");
-            } else {
-                request.getSession().setAttribute("message", "Có lỗi đã xảy ra.<br>"
-                        + "Vui lòng cập nhật lại.<br>");
-                response.sendRedirect("UserInfo?update");
+            String message = "";
+            ValidatingInput check = new ValidatingInput();
+            if (!check.isName(name)) {
+                message += "Tên đã nhập không đúng<br>";
             }
+            if (!check.isPhoneNumber(phone)) {
+                message += "Số điện thoại đã nhập không đúng<br>";
+            }
+            System.out.println(phone + " " + check.isPhoneNumber(phone));
+            if (!check.isEmail(email)) {
+                message += "Email đã nhập không đúng<br>";
+            }
+            if (filePart != null) {
+                UploadFile ulF = new UploadFile();
+                image = ulF.uploadFile(request, "profilepic", UPLOAD_DIR);
+            }
+            if (!message.equals("")) {
+                request.getSession().setAttribute("message", message);
+                request.setAttribute("name", name);
+                request.setAttribute("email", email);
+                request.getRequestDispatcher("view/profile.jsp").forward(request, response);
+            } else {
+                // Update input
+                userinfo.setName(name);
+                userinfo.setSex(gender);
+                userinfo.setEmail(email);
+                userinfo.setAddress(address);
+                userinfo.setImage(image);
+                userinfo.setBday(bod);
+                userinfo.setFirstTimeLogin(true);
+//                UserInfo userinfo = new UserInfo(bday, gender, email, image, address, name, true);
+                Account account = (Account) request.getSession().getAttribute("account");
+                int status = uiD.UpdateUserInfo(userinfo, account.getUserName());
+                if (status > 0) {
+                    request.getSession().setAttribute("userinfo", userinfo);
+                    response.sendRedirect("UserInfo");
+                } else {
+                    request.getSession().setAttribute("message", "Có lỗi đã xảy ra.<br>"
+                            + "Vui lòng cập nhật lại.<br>");
+                    response.sendRedirect("UserInfo?update");
+                }
+            }
+
         }
     }
 
@@ -106,10 +163,10 @@ public class UserInfoController extends HttpServlet {
             fileName = timestamp.getTime() + getFileExtension(getFileName(filePart));
             String applicationPath = request.getServletContext().getRealPath("");
             String basePath = applicationPath + File.separator + UPLOAD_DIR + File.separator;
-            
+
             InputStream inputStream = null;
             OutputStream outputStream = null;
-            
+
             try {
                 File outputFilePath = new File(basePath + fileName);
                 inputStream = filePart.getInputStream();
@@ -134,23 +191,23 @@ public class UserInfoController extends HttpServlet {
         }
         return fileName;
     }
-    
+
     private String getFileName(Part part) {
         final String partHeader = part.getHeader("content-disposition");
         System.out.println("*****partHeader: " + partHeader);
-        for(String content : part.getHeader("content-disposition").split(";")) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
             if (content.trim().startsWith("filename")) {
                 return content.substring(content.indexOf("=") + 1).trim().replace("\"", "");
             }
         }
         return null;
     }
-    
+
     private String getFileExtension(String fileName) {
         String extension = ".";
-        
+
         int i = fileName.lastIndexOf(".");
-        
+
         if (i > 0) {
             extension += fileName.substring(i + 1);
         }
