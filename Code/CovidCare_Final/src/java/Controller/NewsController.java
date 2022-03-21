091422d824dc0b32b85c62b7b821eb43e1bfa241
chinhldc <chinhldse04423@fpt.edu.vn>
2022-03-21 18:09:43 +0700

    [chinhld] update CovidCare_Final news

--- Code/CovidCare_Final/src/java/Controller/NewsController.java
+++ Code/CovidCare_Final/src/java/Controller/NewsController.java
@@ -7,26 +7,40 @@ package Controller;
 
 import DAO.NewsDAO;
 import Model.News;
+import Model.NewsCategory;
 import Model.User;
+import Utils.UploadFile;
 import Utils.ValidatingInput;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 /**
  *
  * @author User
  */
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 10,
+        maxFileSize = 1024 * 1024 * 50,
+        maxRequestSize = 1024 * 1024 * 100
+)
+
 public class NewsController extends HttpServlet {
-    
+
+    private static final String UPLOAD_DIR = "images/uploads";
+
     private final ValidatingInput check = new ValidatingInput();
-    
+
     private final NewsDAO newsDAO = new NewsDAO();
-    
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -44,7 +58,7 @@ public class NewsController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet NewsController</title>");            
+            out.println("<title>Servlet NewsController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet NewsController at " + request.getContextPath() + "</h1>");
@@ -68,30 +82,81 @@ public class NewsController extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        
+
         HttpSession session = request.getSession();
-        
+
         User user = (User) session.getAttribute("user");
-        
+
         String action = request.getParameter("action");
         String id = request.getParameter("id");
-        
+        String cateId = request.getParameter("cateId");
+
         String message = "";
-        
-        News news = newsDAO.getNewsById(Integer.parseInt(id));
-        
-        if (action == null || action.equals("preview")) {
-            if (id == null || !check.isNumber(id)) {
-                message = "Có lỗi đã xảy ra.";
-                request.setAttribute("message", message);
-                request.getRequestDispatcher("error.jsp").forward(request, response);
+
+        News news;
+
+        ArrayList<NewsCategory> categories = (ArrayList) session.getAttribute("newsCate");
+        if (categories == null) {
+            categories = newsDAO.getCateList();
+            session.setAttribute("newsCate", categories);
+        }
+
+        if (user == null || user.getUserType() != 1) {
+            if (action == null) {
+                if (id == null && cateId == null) {
+                    request.setAttribute("title", "Tin tức");
+                    request.getRequestDispatcher("/views/news_list.jsp").forward(request, response);
+                } else {
+                    if (id != null) {
+                        news = newsDAO.getNewsForReader(Integer.parseInt(id));
+
+                        if (news == null) {
+                            session.setAttribute("title", "Tin tức");
+                            sendRedirectError(request, response, "message", "Không tìm thấy bài viết");
+                        } else {
+                            session.setAttribute("title", news.getTitle());
+                            session.setAttribute("news", news);
+                            request.getRequestDispatcher("/views/news_detail.jsp").forward(request, response);
+                        }
+                    } else {
+                        ArrayList<News> newsList = new ArrayList<>();
+                        if (cateId == null) {
+                            newsList = newsDAO.getTopNewsByCategoryId();
+                            request.setAttribute("title", "Tin tức");
+                        } else {
+                            newsList = newsDAO.getNewsListByCategoryId(Integer.parseInt(cateId));
+                            request.setAttribute("title", newsDAO.getCateName(Integer.parseInt(cateId)));
+                        }
+                        request.setAttribute("newsList", newsList);
+                        request.getRequestDispatcher("views/news_list.jsp").forward(request, response);
+                    }
+                }
             } else {
-                request.setAttribute("news", news);
-                request.getRequestDispatcher("news_view.jsp").forward(request, response);
+                request.setAttribute("title", "Tin tức");
+                sendRedirectError(request, response, "message", "Bạn không có quyền truy cập");
             }
         } else {
-            if (action.equals("add") || action.equals("update")) {
-                
+            if (action == null) {
+                ArrayList<News> newsList = newsDAO.getAllNews();
+                request.setAttribute("newsList", newsList);
+                request.setAttribute("title", "Quản lý Tin tức");
+                request.getRequestDispatcher("/views/admin/news_manage.jsp").forward(request, response);
+            } else {
+                if (action.equals("add")) {
+                    request.setAttribute("title", "Thêm tin mới");
+                    request.getRequestDispatcher("/views/admin/news_edit.jsp").forward(request, response);
+                }
+                if (action.equals("update")) {
+                    request.setAttribute("title", "Cập nhật");
+                    news = newsDAO.getNewsById(Integer.parseInt(id));
+
+                    if (news == null) {
+                        sendRedirectError(request, response, "message", "Không tìm thấy bài viết");
+                    } else {
+                        request.setAttribute("news", news);
+                        request.getRequestDispatcher("/views/admin/news_edit.jsp").forward(request, response);
+                    }
+                }
             }
         }
     }
@@ -110,6 +175,86 @@ public class NewsController extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
+
+        HttpSession session = request.getSession();
+
+        User user = (User) session.getAttribute("user");
+
+        String action = request.getParameter("action");
+        String id = request.getParameter("id");
+
+        News news = null;
+
+        if (id == null) {
+            news = new News(0, 0, 0, "", "", "", "", true, null, null, 0);
+        } else if (check.isNumber(id)) {
+            news = newsDAO.getNewsById(Integer.parseInt("id"));
+        } else {
+            sendRedirectError(request, response, "message", "Không tìm thấy bài viết");
+        }
+
+        if (user == null || user.getUserType() != 1) {
+            request.setAttribute("message", "Bạn không có quyền truy cập");
+            request.getRequestDispatcher("/views/error.jsp").forward(request, response);
+        } else {
+            if (action.equals("add") || action.equals("update")) {
+                String title = request.getParameter("title");
+
+                int cateId = 0;
+                if (check.isNumber(request.getParameter("category"))) {
+                    cateId = Integer.parseInt(request.getParameter("category"));
+                }
+
+                // Profile Image processing
+                Part filePart = request.getPart("newsPhoto");
+                String image = news.getPhoto();
+
+                String shortDes = request.getParameter("description");
+
+                String content = request.getParameter("content");
+
+                boolean isPublish = (request.getParameter("publishStatus").equals("1"));
+//                News processing
+                if (news.getCreator() == 0) {
+                    news.setCreator(user.getUserId());
+                }
+                news.setCateId(cateId);
+                news.setTitle(title);
+                news.setContent(content);
+                news.setShort_des(shortDes);
+                UploadFile ulF = new UploadFile();
+                if (!ulF.getFileName(filePart).equals("")) {
+                    image = ulF.uploadFile(request, "newsPhoto", UPLOAD_DIR);
+                }
+                news.setPhoto(image);
+                news.setStatus(isPublish);
+                if (news.getCreate_date() == null) {
+                    news.setCreate_date(new Timestamp(System.currentTimeMillis()));
+                }
+                if (isPublish && news.getPublish_date() == null) {
+                    news.setPublish_date(new Timestamp(System.currentTimeMillis()));
+                }
+
+                int success = -1;
+                if (news.getCateId() == 0) {
+                    success = newsDAO.addNews(news);
+                } else {
+                    success = newsDAO.updateNews(news);
+                }
+                if (success > -1) {
+                    response.sendRedirect("news");
+                } else {
+                    request.setAttribute("message", "Đã có lỗi xảy ra<br>Vui lòng thử lại.");
+                    request.setAttribute("news", news);
+                    request.getRequestDispatcher("/views/admin/news_edit.jsp").forward(request, response);
+                }
+            }
+        }
+    }
+
+    private void sendRedirectError(HttpServletRequest request, HttpServletResponse response, String attribute, String message) throws ServletException, IOException {
+        request.setAttribute(attribute, message);
+        request.getRequestDispatcher("/views/error.jsp").forward(request, response);
     }
 
     /**
---
+++ Code/CovidCare_Final/web/views/admin/news_manage.jsp
@@ -0,0 +1,118 @@
+<%-- 
+    Document   : manageNews
+    Created on : Mar 10, 2022, 9:29:23 PM
+    Author     : Hoang Hung
+--%>
+
+<%@ page language="java" contentType="text/html"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+
+    <%@include file = "../../common/head.jsp" %>
+
+    <body class="body-wrapper">
+
+        <%@include file = "../../common/nav.jsp" %>
+
+        <section class="dashboard section">
+            <!-- Container Start -->
+            <div class="container">
+                <!-- Row Start -->
+                <div class="row">
+                    <div class="col-md-10 offset-md-1 col-lg-3 offset-lg-0" >
+                        <div class="sidebar" >				
+                            <!-- Dashboard Links -->
+                            <div class="widget user-dashboard-menu" style="width:auto;">
+                                <ul>
+                                    <li>
+                                        <a href="news?action=add">
+                                            <i class=""></i> Add News 
+                                        </a>
+                                    </li>    
+                                    <li class="active">
+                                        <a href="">
+                                            <i class=""></i> News List 
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="ManageAccount">
+                                            <i class=""></i> Manager profiles
+                                        </a>
+                                    </li>																			
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-10 offset-md-1 col-lg-9 offset-lg-0" >
+                        <!-- Recently Favorited -->
+                        <div class="widget dashboard-container my-adslist" style="width:auto;">
+                            <h3 class="widget-header">Manage News</h3>
+                            <table class="table table-responsive product-dashboard-table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th class="text-center">Title</th>                                   
+                                        <th class="text-center">Category</th>
+                                        <th class="text-center">Status</th>
+                                        <th class="text-center">Created date</th>
+                                        <th class="text-center">Published date</th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <c:forEach items="${newsList}" var="n">
+                                        <tr>
+
+                                            <td class="">
+                                                <a>${n.getNewsId()}</a>
+                                            <td  class="text-center" >${n.getTitle()}</td>
+                                            <td class="text-center">
+                                                <c:forEach items="${newsCate}" var="cate">
+                                                    <c:if test="${cate.getCateId() == n.getCateId()}">
+                                                        ${cate.getCateName()}
+                                                    </c:if>
+                                                </c:forEach>
+                                            </td>
+                                            <td class="text-center">
+                                                <c:choose>
+                                                    <c:when test="${n.isStatus()}">
+                                                        Đang hiển thị
+                                                    </c:when>
+                                                    <c:otherwise>
+                                                        Đã ẩn
+                                                    </c:otherwise>
+                                                </c:choose>
+                                            </td>
+                                            <td class="text-center">${n.getCreate_date()}</td>
+                                            <td class="text-center">${n.getPublish_date()}</td>
+                                            <td class="action" data-title="Action"> 
+                                                <div class="">
+                                                    <ul class="list-inline justify-content-center">
+                                                        <li class="list-inline-item">
+                                                            <a href="EditNews?newsId=${n.newsId}" onclick="return confirm('Bạn muốn chỉnh sửa không?')" class="edit"><i class="fas fa-edit"  title="Edit"></i></a>
+                                                        </li>      
+                                                        <li class="list-inline-item">
+                                                            <a href="NewsDetails?newsId=${n.newsId}" class="edit"><i class="fa-solid fa-eye" title="View"></i></a>
+                                                        </li>                                            
+                                                        <li class="list-inline-item">
+                                                            <a href="DeleteNews?newsId=${n.newsId}" onclick="return confirm('Bạn có muốn xóa tin tức này không?')" class="delete"><i class="fas fa-eraser" title="Delete"></i></a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </c:forEach>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Row End -->
+            </div>
+            <!-- Container End -->
+        </section>
+
+    </body>
+    <%@include file = "../../common/footer.jsp" %>
+</html>
